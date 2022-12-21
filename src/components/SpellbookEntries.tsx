1d16ed2d5deccbfc296aa4471da60d290f3eaf02
firearrow5235 <firearrow5235@gmail.com>
2022-12-21 09:02:46 -0500

    Add ESLint, configure ESLint and Prettier, add Husky for git hooks

--- src/components/SpellbookEntries.tsx
+++ src/components/SpellbookEntries.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react'
+import React from 'react'
 import { Text, View } from 'react-native'
 import { useSpellbooks } from '../hooks/useSpellbooks'
 import SpellbookEntry from './SpellbookEntry'
